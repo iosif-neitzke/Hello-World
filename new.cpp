7bd4145e024ef1a972517d1f56e28fc91f61f1c1
iosif <iosif.neitzke+git@gmail.com>
2011-09-17 22:50:26 -0500

    Illustrated difference between raw pointers and auto_ptrs.  Also added include for C++11 array class.

--- new.cpp
+++ new.cpp
@@ -1,9 +1,17 @@
 #include <memory>
 #include <iostream>
 #include <cstdlib>
+// #include <array> -std=c++0x
+
+struct Object {
+  ~Object() {
+    std::cout << "destructing" << std::endl;
+  }
+};
 
 int main() {
-  std::auto_ptr<int> p( new int(42) );
-  std::cout << *p << std::endl;
+  std::auto_ptr<Object> p( new Object() ); // calls ~Object() at scope end
+  Object* o = new Object(); // does not call ~Object() at scope end
+
   return EXIT_SUCCESS;
 }
